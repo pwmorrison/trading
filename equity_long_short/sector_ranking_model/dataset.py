401fb562126d9a599214ffda1d03e5fa04f66f9b
Paul Morrison <pwmorrison@gmail.com>
2020-07-05 11:12:20 +1000

    Implemented model and got training working.

--- equity_long_short/sector_ranking_model/dataset.py
+++ equity_long_short/sector_ranking_model/dataset.py
@@ -12,10 +12,12 @@ import random
 
 class RankingDataset(Dataset):
 
-    def __init__(self, tickers_dir, fundamentals_filename, sectors, series_length, lookback, min_sequence_length, template='*.csv', transform=None,
+    def __init__(self, tickers_dir, fundamentals_filename, sectors, x_length, y_length, lookback, min_sequence_length, template='*.csv', transform=None,
                  start_date=None, end_date=None, fixed_start_date=False, datetime_format='%Y-%m-%d',
                  normalised_returns=False, max_n_files=None):
-        self.series_length = series_length
+        self.x_length = x_length
+        self.y_length = y_length
+        self.series_length = x_length + y_length
         self.lookback = lookback
         self.transform = transform
         self.start_date = start_date
@@ -72,7 +74,7 @@ class RankingDataset(Dataset):
                 # Not enough days before the start date to compute returns.
                 continue
 
-            if len(valid_indices) < series_length:
+            if len(valid_indices) < self.series_length:
                 # Not enough days between the start and end date.
                 continue
 
@@ -126,6 +128,14 @@ class RankingDataset(Dataset):
 
         return df
 
+    def get_x_day_returns(self, close_series):
+        """
+        Calculates the returns from every element to the last element.
+        """
+        final_close = close_series[-1]
+        x_day_returns = (final_close - close_series) / close_series
+        return x_day_returns
+
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
@@ -138,13 +148,16 @@ class RankingDataset(Dataset):
             df_2 = self.read_ticker_df(filename_2)
 
             df = pd.merge(df_1, df_2, how='inner', on='date')
-            df.to_csv('merged.csv')
+            # df.to_csv('merged.csv')
 
             if df.shape[0] < self.series_length:
                 continue
 
             returns_1 = np.array(df['returns_x'])
             returns_2 = np.array(df['returns_y'])
+
+            close_1 = np.array(df['close_x'])
+            close_2 = np.array(df['close_y'])
 
             if self.fixed_start_date:
                 # Get a series at the start date.
@@ -153,15 +166,34 @@ class RankingDataset(Dataset):
                 # Get a random sub-series.
                 start = np.random.randint(0, returns_1.shape[0] - self.series_length)
 
-            def extract_series(returns, start_index):
-                series = returns[start_index: start_index + self.series_length]
+            # x_length = self.series_length // 2
+            # y_length = self.series_length - x_length
+            start_y = start + self.x_length
+
+            def extract_series(returns, start_index, length):
+                series = returns[start_index: start_index + length]
                 # Add extra feature dimension.
                 series = series[..., np.newaxis]
                 # Convert to torch tensor.
                 series = torch.from_numpy(series)
                 return series
-            series_1 = extract_series(returns_1, start)
-            series_2 = extract_series(returns_2, start)
+
+            # Get the return series.
+            series_1 = extract_series(returns_1, start, self.x_length)
+            series_2 = extract_series(returns_2, start, self.x_length)
+
+            # Get the x-day returns.
+            close_series_1 = extract_series(close_1, start, self.x_length)
+            close_series_2 = extract_series(close_2, start, self.x_length)
+            x_day_returns_1 = self.get_x_day_returns(close_series_1)
+            x_day_returns_2 = self.get_x_day_returns(close_series_2)
+
+            # Use the close series to determine y.
+            y_series_1 = extract_series(close_1, start_y, self.y_length)
+            y_series_2 = extract_series(close_2, start_y, self.y_length)
+            y_return_1 = (y_series_1[-1] - y_series_1[0]) / y_series_1[0]
+            y_return_2 = (y_series_2[-1] - y_series_2[0]) / y_series_2[0]
+            y = 1 if y_return_1 > y_return_2 else 0
 
             dates = df.index.values[start: start + self.series_length]
             dates = [str(d) for d in dates]
@@ -169,6 +201,9 @@ class RankingDataset(Dataset):
             sample = {
                 'series_1': series_1,
                 'series_2': series_2,
+                'returns_1': x_day_returns_1,
+                'returns_2': x_day_returns_2,
+                'y': y,
                 'filename_1': filename_1,
                 'filename_2': filename_2,
                 'dates': dates,
@@ -183,10 +218,10 @@ def read_csv(filename, datetime_format):
     return df
 
 
-def create_ranking_dataset(tickers_dir, fundamentals_filename, sectors, series_length, lookback, min_sequence_length,
+def create_ranking_dataset(tickers_dir, fundamentals_filename, sectors, x_length, y_length, lookback, min_sequence_length,
                           start_date=None, end_date=None, fixed_start_date=False,
                           normalised_returns=False, max_n_files=None):
-    dataset = RankingDataset(tickers_dir, fundamentals_filename, sectors, series_length, lookback, min_sequence_length,
+    dataset = RankingDataset(tickers_dir, fundamentals_filename, sectors, x_length, y_length, lookback, min_sequence_length,
                             start_date=start_date, end_date=end_date, fixed_start_date=fixed_start_date,
                             normalised_returns=normalised_returns, max_n_files=max_n_files)
     return dataset
@@ -195,26 +230,34 @@ def create_ranking_dataset(tickers_dir, fundamentals_filename, sectors, series_l
 def test_ranking_dataset():
     tickers_dir = r'D:\projects\trading\mlbootcamp\tickers'
     fundamentals_filename = r'D:\snapshot_df.csv'
-    series_length = 200
+    # series_length = 200
+    x_length = 20
+    y_length = 5
     lookback = 200
-    min_sequence_length = 2 * (series_length + lookback)
+    min_sequence_length = 2 * (x_length + y_length + lookback)
     max_n_files = 100
     start_date = datetime.strptime('2010/01/01', '%Y/%m/%d')
     sectors = ['Healthcare']
 
-    dataset = create_ranking_dataset(tickers_dir, fundamentals_filename, sectors, series_length, lookback, min_sequence_length,
+    dataset = create_ranking_dataset(tickers_dir, fundamentals_filename, sectors, x_length, y_length, lookback, min_sequence_length,
                                      start_date=start_date, max_n_files=max_n_files)
     dataloader = DataLoader(dataset, batch_size=1, shuffle=True, num_workers=0)
 
     for b, batch in enumerate(dataloader):
         series_1 = batch['series_1'].numpy()
         series_2 = batch['series_2'].numpy()
+        returns_1 = batch['returns_1'].numpy()
+        returns_2 = batch['returns_2'].numpy()
         filename_1 = batch['filename_1']
         filename_2 = batch['filename_2']
         dates = batch['dates']
         print(b, series_1.shape, filename_1, filename_2, dates)
+        print('Returns series:')
         print(series_1[0, :20, 0])
         print(series_2[0, :20, 0])
+        print('X-day returns:')
+        print(returns_1[0, :20, 0])
+        print(returns_2[0, :20, 0])
 
         break
 
---
+++ equity_long_short/sector_ranking_model/train.py
@@ -0,0 +1,212 @@
+import argparse
+import json
+from pathlib import Path
+import numpy as np
+from datetime import datetime
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+# import visdom
+from torch.utils.data import DataLoader
+
+import pyro
+import pyro.distributions as dist
+from pyro.infer import SVI, JitTrace_ELBO, Trace_ELBO
+from pyro.optim import Adam
+# from utils.mnist_cached import MNISTCached as MNIST
+# from utils.mnist_cached import setup_data_loaders
+# from utils.vae_plots import mnist_test_tsne, plot_llk, plot_vae_samples
+
+from dataset import create_ranking_dataset
+from model import VAE
+
+
+def train():
+    parser = argparse.ArgumentParser(description='Train VAE.')
+    parser.add_argument('-c', '--config', default='train_config.json', help='Config file.')
+    args = parser.parse_args()
+    print(args)
+    c = json.load(open(args.config))
+    print(c)
+
+    fundamentals_filename = r'D:\snapshot_df.csv'
+    sectors = ['Healthcare']
+
+    # clear param store
+    pyro.clear_param_store()
+
+    # batch_size = 64
+    # root_dir = r'D:\projects\trading\mlbootcamp\tickers'
+    # series_length = 60
+    lookback = 50  # 160
+    input_dim = 1
+
+    train_start_date = datetime.strptime(c['train_start_date'], '%Y/%m/%d')
+    train_end_date = datetime.strptime(c['train_end_date'], '%Y/%m/%d')
+    val_start_date = datetime.strptime(c['val_start_date'], '%Y/%m/%d')
+    val_end_date = datetime.strptime(c['val_end_date'], '%Y/%m/%d')
+    min_sequence_length_train = 2 * (c['series_length'] + lookback)
+    min_sequence_length_test = 2 * (c['series_length'] + lookback)
+    max_n_files = None
+
+    out_path = Path(c['out_dir'])
+    out_path.mkdir(exist_ok=True)
+
+    dataset_train = create_ranking_dataset(c['in_dir'], fundamentals_filename, sectors, c['x_length'], c['y_length'], lookback, min_sequence_length_train,
+                                          start_date=train_start_date, end_date=train_end_date,
+                                          normalised_returns=c['normalised_returns'], max_n_files=max_n_files)
+    dataset_val = create_ranking_dataset(c['in_dir'], fundamentals_filename, sectors, c['x_length'], c['y_length'], lookback, min_sequence_length_test,
+                                        start_date=val_start_date, end_date=val_end_date, fixed_start_date=True,
+                                        normalised_returns=c['normalised_returns'], max_n_files=max_n_files)
+    train_loader = DataLoader(dataset_train, batch_size=c['batch_size'], shuffle=True, num_workers=0, drop_last=True)
+    val_loader = DataLoader(dataset_val, batch_size=c['batch_size'], shuffle=False, num_workers=0, drop_last=True)
+
+    N_train_data = len(dataset_train)
+    N_val_data = len(dataset_val)
+    N_mini_batches = N_train_data // c['batch_size']
+    N_train_time_slices = c['batch_size'] * N_mini_batches
+
+    print(f'N_train_data: {N_train_data}, N_val_data: {N_val_data}')
+
+    # setup the VAE
+    vae = VAE(c['x_length'] * 2, z_dim=c['z_dim'], use_cuda=c['cuda'])
+
+    # setup the optimizer
+    adam_args = {"lr": c['learning_rate']}
+    optimizer = Adam(adam_args)
+
+    # setup the inference algorithm
+    elbo = JitTrace_ELBO() if c['jit'] else Trace_ELBO()
+    svi = SVI(vae.model, vae.guide, optimizer, loss=elbo)
+
+    if c['checkpoint_load']:
+        checkpoint = torch.load(c['checkpoint_load'])
+        vae.load_state_dict(checkpoint['model_state_dict'])
+        # optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
+    train_elbo = []
+    val_elbo = []
+    # training loop
+    for epoch in range(c['n_epochs']):
+        # initialize loss accumulator
+        epoch_loss = 0.
+        # do a training epoch over each mini-batch x returned
+        # by the data loader
+        for batch in train_loader:
+            x_series_1 = batch['series_1']
+            x_series_2 = batch['series_2']
+            x_returns_1 = batch['returns_1']
+            x_returns_2 = batch['returns_2']
+            x_1 = torch.cat([x_series_1, x_returns_1], dim=1)
+            x_2 = torch.cat([x_series_2, x_returns_2], dim=1)
+            y = batch['y']
+            # if on GPU put mini-batch into CUDA memory
+            if c['cuda']:
+                x_1 = x_1.cuda()
+                x_2 = x_2.cuda()
+                y = y.cuda()
+            # do ELBO gradient and accumulate loss
+            epoch_loss += svi.step(x_1.float(), x_2.float(), y.float())
+
+        # report training diagnostics
+        normalizer_train = len(train_loader.dataset)
+        total_epoch_loss_train = epoch_loss / normalizer_train
+        train_elbo.append(total_epoch_loss_train)
+        print("[epoch %03d]  average training loss: %.4f" % (epoch, total_epoch_loss_train))
+
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': vae.state_dict(),
+            # 'optimizer_state_dict': optimizer.state_dict(),
+            'train_loss': epoch_loss
+        }, out_path / c['checkpoint_save'].format(epoch))
+
+        if epoch % c['val_frequency'] == 0:
+            val_loss = 0.
+
+            # Compute the loss over the validation set.
+            for i, batch in enumerate(val_loader):
+                x_series_1 = batch['series_1']
+                x_series_2 = batch['series_2']
+                x_returns_1 = batch['returns_1']
+                x_returns_2 = batch['returns_2']
+                x_1 = torch.cat([x_series_1, x_returns_1], dim=1)
+                x_2 = torch.cat([x_series_2, x_returns_2], dim=1)
+                y = batch['y']
+                # if on GPU put mini-batch into CUDA memory
+                if c['cuda']:
+                    x_1 = x_1.cuda()
+                    x_2 = x_2.cuda()
+                    y = y.cuda()
+                x_1 = x_1.float()
+                x_2 = x_2.float()
+                y = y.float()
+                # compute ELBO estimate and accumulate loss
+                val_loss += svi.evaluate_loss(x_1, x_2, y)
+
+                if 1:
+                    # Make some rank predictions.
+                    z_1_loc, z_1_scale, z_1_sample = vae.encode_x(x_1)
+                    z_2_loc, z_2_scale, z_2_sample = vae.encode_x(x_2)
+                    z_loc = torch.cat([z_1_loc, z_2_loc], dim=1)
+                    print(f'y: {y}, z_loc: {z_loc}')
+                    return
+
+                if 0:
+                    if i == 0:
+                        # Visualise first batch.
+                        x_reconst = vae.reconstruct_img(x)
+                        x = x.cpu().numpy()
+                        x_reconst = x_reconst.cpu().detach().numpy()
+
+                        n = min(5, x.shape[0])
+                        fig, axes = plt.subplots(n, 1, squeeze=False)
+                        for s in range(n):
+                            ax = axes[s, 0]
+                            ax.plot(x[s])
+                            ax.plot(x_reconst[s])
+                        fig.savefig(out_path / f'val_{epoch:03d}.png')
+                        plt.close(fig)
+
+            # report test diagnostics
+            normalizer_val = len(val_loader.dataset)
+            total_epoch_loss_val = val_loss / normalizer_val
+            val_elbo.append(total_epoch_loss_val)
+            print("[epoch %03d]  average val loss: %.4f" % (epoch, total_epoch_loss_val))
+
+            if 0:
+                # t-SNE.
+                all_z_latents = []
+                for batch in val_loader:
+                    x = batch['series_1']
+                    # z_latents = minibatch_inference(dmm, test_batch)
+                    # z_latents = encode_x_to_z(dmm, test_batch, sample_z_t=False)
+                    # x, z, x_reconst = test_minibatch(dmm, test_batch, args, sample_z=True)
+
+                    if c['cuda']:
+                        x = x.cuda()
+
+                    z_loc, z_scale, z = vae.encode_x(x.float())
+                    all_z_latents.append(z.cpu().numpy())
+
+                # all_latents = torch.cat(all_z_latents, dim=0)
+                all_latents = np.concatenate(all_z_latents, axis=0)
+
+                # Run t-SNE with 2 output dimensions.
+                from sklearn.manifold import TSNE
+                model_tsne = TSNE(n_components=2, random_state=0)
+                # z_states = all_latents.detach().cpu().numpy()
+                z_states = all_latents
+                z_embed = model_tsne.fit_transform(z_states)
+                # Plot t-SNE embedding.
+                fig = plt.figure()
+                plt.scatter(z_embed[:, 0], z_embed[:, 1], s=10)
+
+                fig.savefig(out_path / f'tsne_{epoch:03d}.png')
+                plt.close(fig)
+
+    print('Finished training.')
+
+
+if __name__ == '__main__':
+    train()
