63225e868a74618aba5dcc6b150221532339b4d3
Paul Morrison <pwmorrison@gmail.com>
2020-07-15 17:47:26 +1000

    Numerai ranking VAE dataset, model, and training script.

---
+++ numerai/ranking_vae_model/dataset.py
@@ -0,0 +1,101 @@
+import torch
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+import glob
+from pathlib import Path
+import os
+from datetime import datetime
+import random
+
+
+class RankingDataset(Dataset):
+
+    def __init__(self, filename, max_n_examples=None):
+        print(filename)
+        self.df = pd.read_csv(filename)
+
+        if max_n_examples:
+            self.df = self.df.iloc[:max_n_examples]
+
+        self.id_col = 'id'
+        self.feature_cols = [c for c in self.df if c.startswith("feature")]
+        self.target_col = 'target_kazutsugi'
+
+        print(f'Created dataset with {self.df.shape[0]} examples.')
+
+    def __len__(self):
+        return self.df.shape[0]
+
+    def read_example(self, index):
+        # TODO: extact the features and target separately.
+        # TODO: Normalise features.
+        # TODO: Make target binary 0, 1.
+        row = self.df.iloc[index]
+        id = row[self.id_col]
+        features = np.array(row[self.feature_cols]).astype(float)
+        target = row[self.target_col]
+        return id, features, target
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        indices = random.sample(range(self.df.shape[0]), 2)
+        id_1, features_1, target_1 = self.read_example(indices[0])
+        id_2, features_2, target_2 = self.read_example(indices[1])
+
+        target_class = 1 if target_1 >= target_2 else 0
+
+        sample = {
+            'features_1': torch.from_numpy(features_1),
+            'features_2': torch.from_numpy(features_2),
+            'target_1': target_1,
+            'target_2': target_2,
+            'target_class': target_class,
+            'id_1': id_1,
+            'id_2': id_2,
+        }
+        return sample
+
+
+def read_csv(filename, datetime_format):
+    df = pd.read_csv(filename)
+    df['date'] = pd.to_datetime(df['date'], format=datetime_format)
+    df.set_index('date', inplace=True)
+    return df
+
+
+def create_ranking_dataset(training_filename, max_n_examples=None):
+    dataset = RankingDataset(training_filename, max_n_examples=max_n_examples)
+    return dataset
+
+
+def test_ranking_dataset():
+    training_filename = r'E:\data\numerai\20200714\numerai_datasets\numerai_training_data_small.csv'
+    max_n_examples = 100
+
+    dataset = create_ranking_dataset(training_filename, max_n_examples=max_n_examples)
+    dataloader = DataLoader(dataset, batch_size=4, shuffle=True, num_workers=0)
+
+    for b, batch in enumerate(dataloader):
+        features_1 = batch['features_1'].numpy()
+        features_2 = batch['features_2'].numpy()
+        target_1 = batch['target_1'].numpy()
+        target_2 = batch['target_2'].numpy()
+        target_class = batch['target_class'].numpy()
+        id_1 = batch['id_1']
+        id_2 = batch['id_2']
+        print(b, features_1.shape, features_1)
+        print(b, target_1.shape, target_1)
+        print(b, target_class.shape, target_class)
+        print(b, len(id_1), id_1)
+
+        break
+
+
+if __name__ == '__main__':
+    # test_ticker_dataset()
+    test_ranking_dataset()
+    # main()
---
+++ numerai/ranking_vae_model/train.py
@@ -0,0 +1,171 @@
+import argparse
+import json
+from pathlib import Path
+import numpy as np
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+import pyro
+import pyro.distributions as dist
+from pyro.infer import SVI, JitTrace_ELBO, Trace_ELBO
+from pyro.optim import Adam
+
+from dataset import create_ranking_dataset
+from model import VAE
+
+
+def train():
+    parser = argparse.ArgumentParser(description='Train VAE.')
+    parser.add_argument('-c', '--config', default='train_config.json', help='Config file.')
+    args = parser.parse_args()
+    print(args)
+    c = json.load(open(args.config))
+    print(c)
+
+    # clear param store
+    pyro.clear_param_store()
+
+    input_dim = 1
+    max_n_examples = None
+
+    out_path = Path(c['out_dir'])
+    out_path.mkdir(exist_ok=True)
+
+    dataset_train = create_ranking_dataset(c['training_filename'], max_n_examples=max_n_examples)
+    dataset_val = create_ranking_dataset(c['training_filename'], max_n_examples=max_n_examples)
+    train_loader = DataLoader(dataset_train, batch_size=c['batch_size'], shuffle=True, num_workers=0, drop_last=True)
+    val_loader = DataLoader(dataset_val, batch_size=c['batch_size'], shuffle=False, num_workers=0, drop_last=True)
+
+    N_train_data = len(dataset_train)
+    N_val_data = len(dataset_val)
+    N_mini_batches = N_train_data // c['batch_size']
+    N_train_time_slices = c['batch_size'] * N_mini_batches
+
+    print(f'N_train_data: {N_train_data}, N_val_data: {N_val_data}')
+
+    # setup the VAE
+    vae = VAE(c['num_features'], z_dim=c['z_dim'], use_cuda=c['cuda'])
+
+    # setup the optimizer
+    adam_args = {"lr": c['learning_rate']}
+    optimizer = Adam(adam_args)
+
+    # setup the inference algorithm
+    elbo = JitTrace_ELBO() if c['jit'] else Trace_ELBO()
+    svi = SVI(vae.model, vae.guide, optimizer, loss=elbo)
+
+    if c['checkpoint_load']:
+        checkpoint = torch.load(c['checkpoint_load'])
+        vae.load_state_dict(checkpoint['model_state_dict'])
+        # optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
+    train_elbo = []
+    val_elbo = []
+    # training loop
+    for epoch in range(c['n_epochs']):
+        # initialize loss accumulator
+        epoch_loss = 0.
+        # do a training epoch over each mini-batch x returned
+        # by the data loader
+        for batch in train_loader:
+            features_1 = batch['features_1']
+            features_2 = batch['features_2']
+            target_class = batch['target_class']
+            if c['cuda']:
+                features_1 = features_1.cuda()
+                features_2 = features_2.cuda()
+                target_class = target_class.cuda()
+            # do ELBO gradient and accumulate loss
+            epoch_loss += svi.step(features_1.float(), features_2.float(), target_class.float())
+
+        # report training diagnostics
+        normalizer_train = len(train_loader.dataset)
+        total_epoch_loss_train = epoch_loss / normalizer_train
+        train_elbo.append(total_epoch_loss_train)
+        print("[epoch %03d]  average training loss: %.4f" % (epoch, total_epoch_loss_train))
+
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': vae.state_dict(),
+            # 'optimizer_state_dict': optimizer.state_dict(),
+            'train_loss': epoch_loss
+        }, out_path / c['checkpoint_save'].format(epoch))
+
+        if epoch % c['val_frequency'] == 0:
+            val_loss = 0.
+
+            # Compute the loss over the validation set.
+            for i, batch in enumerate(val_loader):
+                features_1 = batch['features_1']
+                features_2 = batch['features_2']
+                target_class = batch['target_class']
+                # if on GPU put mini-batch into CUDA memory
+                if c['cuda']:
+                    features_1 = features_1.cuda()
+                    features_2 = features_2.cuda()
+                    target_class = target_class.cuda()
+                features_1 = features_1.float()
+                features_2 = features_2.float()
+                target_class = target_class.float()
+                # compute ELBO estimate and accumulate loss
+                val_loss += svi.evaluate_loss(features_1, features_2, target_class)
+
+                if 1:
+                    z_1_loc, z_1_scale, z_1_sample = vae.encode_x(features_1)
+                    z_2_loc, z_2_scale, z_2_sample = vae.encode_x(features_2)
+                    pred = torch.sigmoid(z_1_loc - z_2_loc)
+                    pred[pred >= 0.5] = 1
+                    pred[pred < 0.5] = 0
+                    target_class = torch.unsqueeze(target_class, dim=1)
+                    pred_target = torch.cat([pred, target_class], dim=1)
+                    print(f'pred vs target: {pred_target}')
+
+                    pred = pred.cpu().detach().numpy()
+                    target_class = target_class.cpu().detach().numpy()
+                    pred_correct = (pred == target_class).astype(int)
+                    print(pred_correct)
+                    print(target_class.shape)
+                    accuracy = torch.sum(pred_correct) / target_class.shape[1]
+                    print(accuracy)
+                    return
+
+                if 0:
+                    # Make some rank predictions.
+                    z_1_loc, z_1_scale, z_1_sample = vae.encode_x(x_1)
+                    z_2_loc, z_2_scale, z_2_sample = vae.encode_x(x_2)
+                    z_loc = torch.cat([z_1_loc, z_2_loc], dim=1)
+                    print(f'y: {y}, z_loc: {z_loc}')
+                    return
+
+                if 0:
+                    if i == 0:
+                        # Visualise first batch.
+                        x_reconst = vae.reconstruct_img(x)
+                        x = x.cpu().numpy()
+                        x_reconst = x_reconst.cpu().detach().numpy()
+
+                        n = min(5, x.shape[0])
+                        fig, axes = plt.subplots(n, 1, squeeze=False)
+                        for s in range(n):
+                            ax = axes[s, 0]
+                            ax.plot(x[s])
+                            ax.plot(x_reconst[s])
+                        fig.savefig(out_path / f'val_{epoch:03d}.png')
+                        plt.close(fig)
+
+            # report test diagnostics
+            normalizer_val = len(val_loader.dataset)
+            total_epoch_loss_val = val_loss / normalizer_val
+            val_elbo.append(total_epoch_loss_val)
+            print("[epoch %03d]  average val loss: %.4f" % (epoch, total_epoch_loss_val))
+
+
+    print('Finished training.')
+
+
+if __name__ == '__main__':
+    train()
